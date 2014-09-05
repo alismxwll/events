24a87f99b5be0e8b1ca4c0d02a207a44559d78d1
Andrew Robert Dean Akers <andrewrdakers@gmail.com>
2014-09-05 22:28:31 +0000

    add some styling

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ end
 
 group :test do
   gem 'shoulda-matchers'
+  gem 'capybara'
 end
 
 # group :production do
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -43,6 +43,6 @@ class EventsController < ApplicationController
   
   private
   def event_params
-    params.require(:event).permit(:name, :time, :date, :place)
+    params.require(:event).permit(:name, :time, :place)
   end
 end
