0b9bbccb5b06b22ede89b3a1ab8e38ab4d42362e
Andrew Robert Dean Akers <andrewrdakers@gmail.com>
2014-09-07 22:18:54 +0000

    add user model, controller and routes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140905192704) do
+ActiveRecord::Schema.define(version: 20140907221739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20140905192704) do
     t.string   "name"
     t.string   "place"
     t.datetime "time"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
