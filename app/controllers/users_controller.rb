882e1c980fe75dcee88ce8935bf5c88af58df49e
Andrew Robert Dean Akers <andrewrdakers@gmail.com>
2014-09-07 22:59:01 +0000

    add user sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,16 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
   
-  
-  
-  
-  
-  
-  
-  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect root_url, notice: "Thanks for signing up!"
+    else
+      render "new"
+    end
+  end
   
   private
   def user_params
