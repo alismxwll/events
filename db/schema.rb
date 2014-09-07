548dc7558d40c86ecdf1b1b33730c0deef4d0b57
Andrew Robert Dean Akers <andrewrdakers@gmail.com>
2014-09-07 22:31:55 +0000

    add timestamps to events table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140907221739) do
+ActiveRecord::Schema.define(version: 20140907222313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20140907221739) do
     t.string   "name"
     t.string   "place"
     t.datetime "time"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
